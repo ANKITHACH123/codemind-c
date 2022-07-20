8f89f78cbbfdf79921f0ef3c7848208160bc9c46
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-07-20 10:25:34 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],sum=0,c=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d ",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     avg=(sum/n);     for(i=0;i<n;i++)     {         if(arr[i]>=avg)         {             c+=1;         }     }     printf("%d ",c);
    }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],sum=0,c=0;
+    float avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d ",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    avg=(sum/n);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=avg)
+        {
+            c+=1;
+        }
+    }
+    printf("%d ",c);
+    
+}
